cecfa7ed98941027357c2cebf503a7c388015892
Kenta Eguchi <kenta.eguchi@gmail.com>
2016-07-11 23:18:27 +0900

    Initial Commit

---
+++ EgkUpnpApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EgkUpnpApp
+//
+//  Created by 江口 健太 on 2016/07/11.
+//  Copyright © 2016年 egk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
